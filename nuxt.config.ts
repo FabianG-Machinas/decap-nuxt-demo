e93b891d993acc55cfae2988f0389d0ea886b8ae
Fabian Ghena <fabian.g@machinas.com>
2025-02-26 16:00:40 +0200

    set ssr to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  ssr: false,
+  nitro: {
+    prerender: {
+      routes: ['/', '/admin'], // Exclude `/admin/`
+    },
+  }
 })
