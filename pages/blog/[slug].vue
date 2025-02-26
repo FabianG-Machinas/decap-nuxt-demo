6d5eabc8d007865c2a010ba6d3580fbb678d2cb2
Fabian Ghena <fabian.g@machinas.com>
2025-02-26 17:01:44 +0200

    fix

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,15 +1,16 @@
-<script setup>
-const route = useRoute();
-const { data: post } = await useAsyncData(() =>
-  queryContent("posts").where({ slug: route.params.slug }).findOne()
-);
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const post = await queryCollection('content').path(route.path).first()
 </script>
 
 <template>
   <div v-if="post">
     <h1>{{ post.title }}</h1>
-    <img :src="post.image" alt="Post Image" />
-    <p>{{ post.date }}</p>
     <div v-html="post.body"></div>
+  </div>
+  <div v-else>
+    <p>Post not found.</p>
   </div>
 </template>
