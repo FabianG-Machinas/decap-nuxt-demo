5be49d49fdabae4c501b889fd202d6c1f146b856
Fabian Ghena <fabian.g@machinas.com>
2025-02-26 15:29:02 +0200

    generate blog posts

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,15 @@
+<script setup>
+const route = useRoute();
+const { data: post } = await useAsyncData(() =>
+  queryContent("posts").where({ slug: route.params.slug }).findOne()
+);
+</script>
+
+<template>
+  <div v-if="post">
+    <h1>{{ post.title }}</h1>
+    <img :src="post.image" alt="Post Image" />
+    <p>{{ post.date }}</p>
+    <div v-html="post.body"></div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Welcome to Nuxt 3 + Decap CMS</h1>
+      <p>Go to <a href="/admin/">/admin/</a> to access Decap CMS.</p>
+    </div>
+  </template>
