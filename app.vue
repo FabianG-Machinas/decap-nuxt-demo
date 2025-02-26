f12baecbb58384d030c1bcf4122119ddfb5f033b
Fabian Ghena <fabian.g@machinas.com>
2025-02-26 11:10:56 +0200

    Added Identity Widget

--- app.vue
+++ app.vue
@@ -4,3 +4,13 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+</script>
